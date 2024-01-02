0a8f0390117a2c428912c9f15eff30d42f92e60d
guterfps <ithayg@gmail.com>
2024-01-02 13:46:52 +0200

    remove zig-cache from repo

--- zig_snake/zig-cache/o/19815703743d97258106f92ddf34a7e4/cimport.h.d
+++
@@ -1,3 +0,0 @@
-cimport.o: \
-  /home/malaka/raylib/snake/zig_snake/zig-cache/o/19815703743d97258106f92ddf34a7e4/cimport.h \
-  /home/malaka/raylib/snake/zig_snake/src/bindings.h
--- zig_snake/zig-cache/o/f3e5a826b66aca5186a86350319eca49/cimport.h.d
+++
@@ -1,3 +0,0 @@
-cimport.o: \
-  /home/malaka/raylib/snake/zig_snake/zig-cache/o/f3e5a826b66aca5186a86350319eca49/cimport.h \
-  /home/malaka/raylib/snake/zig_snake/src/bindings.h
